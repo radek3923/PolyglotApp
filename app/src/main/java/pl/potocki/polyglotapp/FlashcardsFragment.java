805756cc78c536b7ef94371f72136d2fbc8c4af4
Radosław Potocki <radoslaw.potocki3923@gmail.com>
2023-06-20 22:49:03 +0200

    Add rotating left and right side

--- app/src/main/java/pl/potocki/polyglotapp/FlashcardsFragment.java
+++ app/src/main/java/pl/potocki/polyglotapp/FlashcardsFragment.java
@@ -2,7 +2,6 @@ package pl.potocki.polyglotapp;
 
 import android.animation.Animator;
 import android.animation.AnimatorInflater;
-import android.animation.AnimatorListenerAdapter;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -20,25 +19,41 @@ public class FlashcardsFragment extends Fragment {
     private FragmentFlashcardsBinding binding;
     private boolean isFlipped = false;
 
+    // Dodatkowe zmienne do obsługi dwóch animacji
+    private Animator flipRightAnimator;
+    private Animator flipLeftAnimator;
+
     @Override
-    public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
         binding = FragmentFlashcardsBinding.inflate(inflater, container, false);
         return binding.getRoot();
-
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        // Inicjalizacja animacji obracania w prawo
+        flipRightAnimator = AnimatorInflater.loadAnimator(getActivity(), R.animator.flip_right_animation);
+        flipRightAnimator.setTarget(binding.cardContainer);
+        flipRightAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
+
+        // Inicjalizacja animacji obracania w lewo
+        flipLeftAnimator = AnimatorInflater.loadAnimator(getActivity(), R.animator.flip_left_animation);
+        flipLeftAnimator.setTarget(binding.cardContainer);
+        flipLeftAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
+
         binding.cardContainer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                flipCard();
                 if (!isFlipped) {
+                    // Obracanie w prawo
+                    flipRight();
                     isFlipped = true;
+                } else {
+                    // Obracanie w lewo
+                    flipLeft();
+                    isFlipped = false;
                 }
             }
         });
@@ -50,16 +65,12 @@ public class FlashcardsFragment extends Fragment {
         binding = null;
     }
 
-    private void flipCard() {
-        Animator flipAnimator = AnimatorInflater.loadAnimator(getActivity(), R.animator.flip_animation);
-        flipAnimator.setTarget(binding.cardContainer);
-        flipAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
-        flipAnimator.addListener(new AnimatorListenerAdapter() {
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                // Wykonaj akcję po zakończeniu animacji, np. pokaż tłumaczenie
-            }
-        });
-        flipAnimator.start();
+    private void flipRight() {
+        flipRightAnimator.start();
+        System.out.println("Prawo");
+    }
+
+    private void flipLeft() {
+        flipLeftAnimator.start();
     }
 }
