f697c56c37128521a21af0be9302d7cfa3887441
Radosław Potocki <radoslaw.potocki3923@gmail.com>
2023-06-20 22:43:18 +0200

    Add rotating flashcard

--- app/src/main/java/pl/potocki/polyglotapp/FlashcardsFragment.java
+++ app/src/main/java/pl/potocki/polyglotapp/FlashcardsFragment.java
@@ -1,5 +1,8 @@
 package pl.potocki.polyglotapp;
 
+import android.animation.Animator;
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorListenerAdapter;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -8,12 +11,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateDecelerateInterpolator;
 
 import pl.potocki.polyglotapp.databinding.FragmentFlashcardsBinding;
 
 public class FlashcardsFragment extends Fragment {
 
     private FragmentFlashcardsBinding binding;
+    private boolean isFlipped = false;
 
     @Override
     public View onCreateView(
@@ -28,12 +33,33 @@ public class FlashcardsFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-
+        binding.cardContainer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flipCard();
+                if (!isFlipped) {
+                    isFlipped = true;
+                }
+            }
+        });
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    private void flipCard() {
+        Animator flipAnimator = AnimatorInflater.loadAnimator(getActivity(), R.animator.flip_animation);
+        flipAnimator.setTarget(binding.cardContainer);
+        flipAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
+        flipAnimator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                // Wykonaj akcję po zakończeniu animacji, np. pokaż tłumaczenie
+            }
+        });
+        flipAnimator.start();
     }
 }
